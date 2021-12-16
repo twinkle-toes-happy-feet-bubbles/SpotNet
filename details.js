68df0b15941b88238d694b800f85aee75cb67d84
twinkle-toes-happy-feet-bubbles <72191306+twinkle-toes-happy-feet-bubbles@users.noreply.github.com>
2021-12-16 20:46:29 +0530

    Create details.js

---
+++ details.js
@@ -0,0 +1,19 @@
+// modal add
+
+const addModal = document.querySelector('.add-modal');
+
+const addModalForm = document.querySelector('.add-modal .form');
+
+// modal edit
+
+const editModal = document.querySelector('.edit-modal');
+
+const editModalForm = document.querySelector('.edit-modal .form');
+
+const btnAdd = document.querySelector('.btn-add');
+
+const tableUsers = document.querySelector('.table-products');
+
+let id;
+
+console.log("Reading");
